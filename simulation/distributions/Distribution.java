d0b7370472a00dd65c9b7d1f5daeb6b8867e12a0
ykk <yapkokkiong@gmail.com>
2006-05-04 15:51:07 +0000

    Finished Poisson

--- simulation/distributions/Distribution.java
+++ simulation/distributions/Distribution.java
@@ -6,10 +6,14 @@ package simulation.distributions;
 public abstract class Distribution
 {
     /** Return if the distribution is discrete.
+     * @return if distribution is discrete
+     * @see #isContinuous()
      */
     public abstract boolean isDiscrete();
 
     /** Return if the distribution is continuous.
+     * @return if distribution is continuous
+     * @see #isDiscrete()
      */
     public boolean isContinuous()
     {
@@ -19,4 +23,17 @@ public abstract class Distribution
     /** Get instance of random distribution.
      */
     public abstract double getInstance();
+
+    /** Return probability of distribution lies within specified interval.
+     * @param startValue value starting range under consideration (included)
+     * @param endValue value ending range under consideration (excluded)
+     * @return probability of distribution lies within startValue and endValue
+     */
+    public abstract double density(double startValue, double endValue);
+
+    /** Return probability of distribution being below the specified value.
+     * @param value value to consider
+     * @return probability of distribution being less than value
+     */
+    public abstract double cumulativeDensity(double value);
 }
