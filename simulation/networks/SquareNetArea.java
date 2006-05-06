dfee55213bcd5bd921d1ce1ae52523458156f8c6
ykk <yapkokkiong@gmail.com>
2006-05-06 12:26:07 +0000

    Added square network

---
+++ simulation/networks/SquareNetArea.java
@@ -0,0 +1,17 @@
+package simulation.networks;
+
+/** Class to provide square area definition for a network.
+ * @author ykk
+ */
+public class SquareNetArea
+    extends RectangleNetArea
+{
+    /** Constructor to create square network.
+     * Network has x and y coordinates for 0 to specified maxima.
+     * @param max maximum x and y coordinate
+     */
+    public SquareNetArea(double max)
+    {
+	super(max,max);
+    }    
+}
