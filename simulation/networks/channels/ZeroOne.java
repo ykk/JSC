4d729b6d26031251cb765519cde446ae158b64b4
ykk <ykk@cbece23a-f012-0410-9c50-f0812ac77975>
2006-05-11 16:48:42 +0000

    Added channel
    git-svn-id: svn://kkmobile.homelinux.com/jsc@27 cbece23a-f012-0410-9c50-f0812ac77975

---
+++ simulation/networks/channels/ZeroOne.java
@@ -0,0 +1,32 @@
+package simulation.networks.channels;
+
+import simulation.networks.nodes.*;
+
+/** Class for a zero-one channel.
+ * @author ykk
+ */
+public class ZeroOne
+    extends Channel
+{
+    //Members
+    /** Threshold distance.
+     * Within the distance, nodes can communicate reliably, 
+     * else no reception can be performed.
+     */
+    public double thresholdDistance;
+
+    //Methods
+    /** Check if source node can transmit directly to destination.
+     */
+    public ZeroOne(double thresholdDistance)
+    {
+	this.thresholdDistance = thresholdDistance;
+    }
+
+    public boolean canTx(Node source, Node destination)
+    {
+	return (Math.sqrt(Math.pow(source.x - destination.x,2.0)+
+			  Math.pow(source.y - destination.y,2.0))
+		< thresholdDistance);
+    }
+}
