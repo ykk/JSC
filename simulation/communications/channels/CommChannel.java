aed09922edfe526d536c4e28e86e4ceac122fd43
ykk <yapkokkiong@gmail.com>
2006-05-18 06:55:26 +0000

    Added communication

--- simulation/communications/channels/CommChannel.java
+++ simulation/communications/channels/CommChannel.java
@@ -2,10 +2,10 @@ package simulation.networks.channels;
 
 import simulation.networks.nodes.*;
 
-/** Interface for a communication channel.
+/** Abstract class for a communication channel.
  * @author ykk
  */
-public interface CommChannel
+public abstract class CommChannel
 {
     /** Transmit packet from source to destination.
      * @param source source node
@@ -13,5 +13,5 @@ public interface CommChannel
      * @param packet packet to transmit
      * @return if packet is successfully transmitted
      */
-    public boolean transmit(Node source, Node destination, Object packet);
+    public abstract boolean transmit(Node source, Node destination, Object packet);
 }
