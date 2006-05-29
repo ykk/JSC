82dfc74e2d197cd9b415db1c7a85a0914824c22d
ykk <yapkokkiong@gmail.com>
2006-05-29 14:07:05 +0000

    ALOHA done, but test simulation to be written

--- simulation/communications/channels/CommChannel.java
+++ simulation/communications/channels/CommChannel.java
@@ -7,6 +7,29 @@ import simulation.communications.nodes.*;
  */
 public abstract class CommChannel
 {
+    //Members
+    /** Transmission rate (in bytes per second).
+     */
+    public double rate;
+
+    //Methods
+    /** Constructor to generate communication channel.
+     * @param rate transmission rate
+     */
+    public CommChannel(double rate)
+    {
+	this.rate = rate;
+    }
+
+    /** Return transmission duration required for n bytes.
+     * @param n number of bytes to transmit
+     * @return duration required to transmit bytes
+     */
+    public double transmitDuration(int n)
+    {
+	return ((double) n)/rate;
+    }
+
     /** Transmit packet from source to destination.
      * @param source source node
      * @param destination destination node
--- simulation/eventbased/mediumaccess/ALOHA.java
+++ simulation/eventbased/mediumaccess/ALOHA.java
@@ -9,6 +9,7 @@ import simulation.networks.*;
 import simulation.networks.nodes.*;
 import simulation.networks.channels.*;
 import simulation.utilities.structures.*;
+import simulation.distributions.*;
 
 /** Class to implement ALOHA medium access control.
  * @author ykk
@@ -17,23 +18,47 @@ public class ALOHA
     extends MACNode
 {
     //Members
-    /** Array of states node can be in.
-     * States = "Idle", "Waiting", "Transmitting", "Receiving", "Collided"
-     */
-    public static final String[] states = {"Idle", "Waiting", "Transmitting", "Receiving", "Collided"};
     /** Current state of node.
-     * @see #states
+     * @see #stateIdle
+     * @see #stateWaiting
+     * @see #stateTransmitting
+     * @see #stateReceiving 
+     * @see #stateCollided
      */
-    public int state = 0; //Idle
+    public int state = stateIdle; //Idle
+    /** Idle state.
+     */
+    public static final int stateIdle = 0;
+    /** Waiting state.
+     */
+    public static final int stateWaiting = 1;
+    /** Transmitting state.
+     */
+    public static final int stateTransmitting = 2;
+    /** Receiving state.
+     */
+    public static final int stateReceiving = 3;
+    /** Collided state.
+     */
+    public static final int stateCollided = 4;
     /** Array of events node can process.
-     * Events = "Receive Ended", 
-     *          "Transmission Ended", 
-     *          "Wait Ended"
+     * Events = 0 "Receive Ended",
+     *          1 "Transmission Ended", 
+     *          2 "Wait Ended"
      */
     public static final String[] events = {"Receive Ended", "Transmission Ended", "Wait Ended"};
     /** Number of ongoing receptions.
      */
     private int onGoing = 0;
+    /** Indicate of transmitting.
+     */
+    private boolean isTransmitting = false;
+    /** Distribution of random wait.
+     */
+    Distribution waitTime;
+    /** Currently receiving packet.
+     */
+    Object packet;
 
     //Methods
     /** Event triggered interface.
@@ -43,17 +68,42 @@ public class ALOHA
      * @param event event string definition
      * @param simulator reference to simulator
      * @see #events
-     * @see #states
+     * @see #state
+     * @see #receive(CommNode source, Object packet, Simulator simulator)
+     * @see #trigger(Simulator simulator)
      */
     public void run(double time, String event, Simulator simulator)
     {
 	switch (Array.indexOf(events, event))
 	{
 	case 0: //Receive Ended
+	    onGoing--;
+	    if (!isTransmitting & (onGoing == 0))
+	    {
+		state = stateWaiting;
+		simulator.add(new Event(simulator.time()+waitTime.getInstance(),
+					this,
+					this.events[2])); //Wait Ended Scheduled
+	    }
+	    if (state != stateCollided)
+		queue.receive(packet);
 	    break;
 	case 1: //Transmission Ended
+	    isTransmitting = false;
+	    if (onGoing == 0)
+	    {
+		state = stateWaiting;
+		simulator.add(new Event(simulator.time()+waitTime.getInstance(),
+					this,
+					this.events[2])); //Wait Ended Scheduled
+	    }
 	    break;
 	case 2: //Wait Ended
+	    Object packet = queue.get();
+	    if (packet != null)
+		startTransmission((Packet) packet, simulator);
+	    else
+		state = stateIdle;
 	    break;
 	default:
 	    throw new RuntimeException(this+" encounters unknown event "+event);
@@ -65,15 +115,35 @@ public class ALOHA
      */
     public void startTransmission(Packet packet, Simulator simulator)
     {
+	if (onGoing != 0)
+	    state = stateCollided;
+	else
+	    state = stateTransmitting;
+	isTransmitting = true;
+	simulator.add(new Event(simulator.time()+
+				commChannel.transmitDuration(packet.totalLength()),
+				this,
+				this.events[1])); //Transmission Ended scheduled
     }
 
     public void receive(CommNode source, Object packet, Simulator simulator)
     {
+	if ((state == stateTransmitting) || 
+	    (state == stateReceiving) || (state == stateCollided))
+	    state = stateCollided;
+	else
+	    state = stateReceiving;
+	onGoing++;
+	simulator.add(new Event(simulator.time()+
+				commChannel.transmitDuration(((Packet) packet).totalLength()),
+				this,
+				this.events[0])); //Receive Ended scheduled
+	this.packet = packet;
     }
 
     public void trigger(Simulator simulator)
     {
-	if (state == 0)  //Idle state
+	if (state == stateIdle)
 	{
 	    Object packet = queue.get();
 	    if (packet != null)
@@ -83,11 +153,14 @@ public class ALOHA
 
     public Node newNode(Coordinate coordinate)
     {
-	return new ALOHA(coordinate, this.channel, this.commChannel, this.queue);
+	return new ALOHA(coordinate, this.channel, this.commChannel, 
+			 this.queue, this.waitTime);
     }
 
-    public ALOHA(Coordinate coordinate, Channel channel, CommChannel commChannel, Queue queue)
+    public ALOHA(Coordinate coordinate, Channel channel, CommChannel commChannel, 
+		 Queue queue, Distribution waitTime)
     {
 	super(coordinate, channel, commChannel, queue);
+	this.waitTime = waitTime;
     }
 }
