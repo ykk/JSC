cfb839b57453bbe3b7c7020e8951d6aa51a77667
ykk <ykk@cbece23a-f012-0410-9c50-f0812ac77975>
2006-05-30 06:48:41 +0000

    Add node operation
    git-svn-id: svn://kkmobile.homelinux.com/jsc@49 cbece23a-f012-0410-9c50-f0812ac77975

---
+++ simulation/utilities/nodefilters/NodeOperation.java
@@ -0,0 +1,10 @@
+package simulation.utilities.nodefilters;
+
+import java.util.*;
+/** Node filter to perform operations between set of nodes.
+ * @author ykk
+ */
+public class NodeOperation
+{
+    
+}
