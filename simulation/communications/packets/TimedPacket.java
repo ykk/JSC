2f948ef3f65b856ce9d6a7e5e6f964d6b0514a6d
ykk <yapkokkiong@gmail.com>
2007-02-21 03:47:45 +0000

    Added timed packet

---
+++ simulation/communications/packets/TimedPacket.java
@@ -0,0 +1,55 @@
+package simulation.communications.packets;
+
+/** Class for communication packet with payload and time.
+ * @author ykk
+ */
+public class TimedPacket
+    extends Packet
+{
+    //Members
+    /** Start time of packet.
+     */
+    public double startTime=0;
+    /** End time of packet.
+     */
+    public double endTime=0;
+
+    //Methods
+    /** Constructor for packet.
+     * @param length length of payload
+     */
+    public TimedPacket(int length)
+    {
+	super(length);
+    }
+
+    /** Constructor for packet with start time.
+     * @param length length of payload
+     * @param startTime start time of packet
+     */
+    public TimedPacket(int length, double startTime)
+    {
+	super(length);
+	this.startTime = startTime;
+    }
+
+    /** Record end time of packet.
+     * @param endTime end time of packet
+     */
+    public void recordEnd(double endTime)
+    {
+	this.endTime = endTime;
+    }
+    
+    /** Return delay of packet.
+     * @return delay of packet
+     */
+    public double delay()
+    {
+	if (endTime <= startTime)
+	    throw new RuntimeException(this+" has delay "+(endTime-startTime)+
+				       " which is less than or equal to zero");
+	else
+	    return endTime-startTime;
+    }
+}
