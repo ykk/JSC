32c380adc093a9a8f802e1d3224bd95da4e8dfbe
ykk <ykk@cbece23a-f012-0410-9c50-f0812ac77975>
2007-02-23 19:18:29 +0000

    Added TxRxQueue
    git-svn-id: svn://kkmobile.homelinux.com/jsc@72 cbece23a-f012-0410-9c50-f0812ac77975

---
+++ simulation/communications/queues/TxRxQueue.java
@@ -0,0 +1,52 @@
+package simulation.communications.queues;
+
+/** Class encapsulating two queues.
+ * @author ykk
+ */
+public class TxRxQueue
+    extends Queue
+{
+    //Members
+    /** Transmit queue.
+     */
+    Queue transmit;
+    /** Receive queue.
+     */
+    Queue receive;
+
+    //Methods
+    /** Constructor.
+     * @param transmit transmit queue.
+     * @param recieve receive queue.
+     */
+    public TxRxQueue(Queue transmit, Queue receive)
+    {
+	this.transmit = transmit;
+	this.receive = receive;
+    }
+    
+    /** Receive packet into receive queue.
+     * @param packet packet to be received by queue
+     * @return if packet is dropped
+     */
+    public boolean receive(Object packet)
+    {
+	return receive.receive(packet);
+    }
+
+    /** Get packet from transmit queue.
+     * @return packet to be transmitted; or null if queue is empty
+     */
+    public Object get()
+    {
+	return transmit.get();
+    }
+
+    /** Duplicate queue.
+     * @return new instance of queue
+     */
+    public Queue newQueue()
+    {
+	return new TxRxQueue(transmit.newQueue(), receive.newQueue());
+    }
+}
