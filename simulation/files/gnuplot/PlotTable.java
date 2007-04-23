b2ccaf1b9f7e9f3392e92ef3a9ffefa3eeecb215
ykk <ykk@cbece23a-f012-0410-9c50-f0812ac77975>
2007-04-23 16:06:52 +0000

    Added plotter
    git-svn-id: svn://kkmobile.homelinux.com/jsc@129 cbece23a-f012-0410-9c50-f0812ac77975

---
+++ simulation/files/gnuplot/PlotTable.java
@@ -0,0 +1,15 @@
+package simulation.files.gnuplot;
+
+import java.io.*;
+import java.util.*;
+import simulation.files.text.*;
+
+/** Class to generate {@link Table}.
+ * @author ykk
+ */ 
+public class PlotTable
+{
+    /
+    /**
+     */
+}
