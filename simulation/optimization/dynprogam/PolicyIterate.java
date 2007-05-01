c60b9673e0bb28499cbb0fcd0b5712789df49038
ykk <ykk@cbece23a-f012-0410-9c50-f0812ac77975>
2007-05-01 04:43:36 +0000

    Added DP
    git-svn-id: svn://kkmobile.homelinux.com/jsc@148 cbece23a-f012-0410-9c50-f0812ac77975

---
+++ simulation/optimization/dynprogam/PolicyIterate.java
@@ -0,0 +1,10 @@
+package simulation.optimization.dynprogram;
+
+/** Class to perform dynamic programming policy iteration.
+ * @author ykk
+ */
+public class PolicyIterate
+{
+    /**
+     */
+}
---
+++ simulation/optimization/dynprogam/State.java
@@ -0,0 +1,13 @@
+package simulation.optimization.dynprogram;
+
+import java.util.*;
+
+/** Class to store state of dynamic program.
+ * @author ykk
+ */
+public class State
+{
+    //
+    /**
+     */
+}
