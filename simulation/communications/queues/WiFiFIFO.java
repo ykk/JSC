80569718c2da26b99cf424f4524ded50105f3347
ykk <ykk@cbece23a-f012-0410-9c50-f0812ac77975>
2008-05-16 06:23:37 +0000

    Changes for WiFI
    git-svn-id: svn://kkmobile.homelinux.com/jsc@250 cbece23a-f012-0410-9c50-f0812ac77975

---
+++ simulation/communications/queues/WiFiFIFO.java
@@ -0,0 +1,12 @@
+package simulation.communications.queues;
+
+import simulation.communications.packets.*;
+import java.util.*;
+
+/** Queue wrapper to insert WiFi Frame.
+ * @author ykk
+ */
+public class WiFiFramer
+{
+    
+}
